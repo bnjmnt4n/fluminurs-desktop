926847fce4389a708375206d62c00a0136fd0cf9
Zacchua <zacchua@gmail.com>
2021-05-23 16:44:13 +0800

    Added some styling for the header
    Selected tab is now different colour from the rest of
    the buttons (dark blue to look like luminus lmao).
    
    Also attempted to center the name by pushing it down
    slightly but hardcoded the value so might not work
    properly all the time.

--- src/header.rs
+++ src/header.rs
@@ -1,4 +1,4 @@
-use iced::{button, Button, Command, Element, Row, Text};
+use iced::{button, Button, Command, Element, Row, Text, Rule, Column, Space, Length, VerticalAlignment};
 
 use crate::message::Message;
 use crate::pages::Page;
@@ -10,6 +10,7 @@ pub struct Header {
     multimedia_button: button::State,
     weblectures_button: button::State,
     conferences_button: button::State,
+    active_module: Page,
 }
 
 #[derive(Debug, Clone)]
@@ -25,12 +26,14 @@ impl Header {
             multimedia_button: button::State::new(),
             weblectures_button: button::State::new(),
             conferences_button: button::State::new(),
+            active_module: Page::Modules,
         }
     }
 
     pub fn update(&mut self, message: HeaderMessage) -> Command<Message> {
         match message {
             HeaderMessage::SwitchPage(page) => {
+                self.active_module = page.clone();
                 Command::perform(async { page }, Message::SwitchPage)
             }
         }
@@ -40,34 +43,74 @@ impl Header {
         // TODO: different styles for active module
         let content = Row::new()
             .max_width(800)
-            .spacing(20)
             .push(
                 Button::new(&mut self.modules_button, Text::new("Modules"))
-                    .on_press(HeaderMessage::SwitchPage(Page::Modules)),
+                    .on_press(HeaderMessage::SwitchPage(Page::Modules)).style(match self.active_module { Page::Modules => style::Button::ActiveButton, _ => style::Button::InactiveButton }),
             )
             .push(
                 Button::new(&mut self.files_button, Text::new("Files"))
-                    .on_press(HeaderMessage::SwitchPage(Page::Files)),
-            )
+                    .on_press(HeaderMessage::SwitchPage(Page::Files)).style(match self.active_module { Page::Files => style::Button::ActiveButton, _ => style::Button::InactiveButton }))
             .push(
                 Button::new(&mut self.multimedia_button, Text::new("Multimedia"))
-                    .on_press(HeaderMessage::SwitchPage(Page::Multimedia)),
+                    .on_press(HeaderMessage::SwitchPage(Page::Multimedia)).style(match self.active_module{ Page::Multimedia => style::Button::ActiveButton, _ => style::Button::InactiveButton }),
             )
             .push(
                 Button::new(&mut self.weblectures_button, Text::new("Weblectures"))
-                    .on_press(HeaderMessage::SwitchPage(Page::Weblectures)),
+                    .on_press(HeaderMessage::SwitchPage(Page::Weblectures)).style(match self.active_module{ Page::Weblectures => style::Button::ActiveButton, _ => style::Button::InactiveButton }),
             )
             .push(
                 Button::new(&mut self.conferences_button, Text::new("Conferences"))
-                    .on_press(HeaderMessage::SwitchPage(Page::Conferences)),
+                    .on_press(HeaderMessage::SwitchPage(Page::Conferences)).style(match self.active_module {Page::Conferences => style::Button::ActiveButton, _ => style::Button::InactiveButton}),
             );
 
         let content = if let Some(name) = name {
-            content.push(Text::new(name))
+            content.push(Space::with_width(Length::FillPortion(7))).push(Text::new(name).vertical_alignment(VerticalAlignment::Bottom).height(Length::Units(25)))
         } else {
             content
         };
 
-        content.into()
+        let content2 = Column::new().push(content).push(Rule::horizontal(0).style(style::Divider::Header));
+
+        content2.into()
+    }
+}
+
+mod style {
+    use iced::{ button, rule, Background, Color };
+
+    pub enum Button {
+        ActiveButton,
+        InactiveButton,
+    }
+
+    pub enum Divider {
+        Header,
+    }
+
+    impl button::StyleSheet for Button {
+        fn active(&self) -> button::Style {
+            match self {
+                Button::ActiveButton =>  button::Style {
+                    background: Option::Some(Background::Color( Color {r: 0.0, g: 0.0, b: 0.545, a: 1.0})),
+                    border_radius: 0.0,
+                    border_width: 0.0,
+                    text_color: Color::WHITE,
+                    ..button::Style::default()
+                },
+                Button::InactiveButton => button::Style::default(),
+            }
+
+        }
+    }
+
+    impl rule::StyleSheet for Divider {
+        fn style(&self) -> rule::Style{
+            rule::Style {
+                fill_mode: rule::FillMode::Full,
+                color: Color::BLACK,
+                width: 1,
+                radius: 1.0,
+            }
+        }
     }
 }
